d06d07bb07d81049c64fdaac527533dffd297358
yan.yang <yangyan@zgcszkw.com>
2019-01-23 11:32:51 +0800

    import get,set from mobx

--- MainScreen.js
+++ MainScreen.js
@@ -6,61 +6,56 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {StyleSheet, Button, Text, View} from 'react-native';
-import {inject, observer} from 'mobx-react/native';
-import {computed} from 'mobx';
+import React, { Component } from 'react';
+import { StyleSheet, Button, Text, View, Div } from 'react-native';
+import { inject, observer } from 'mobx-react/native';
+import { observable, computed, get, has } from 'mobx';
 
 @inject('store')
 @observer
 export default class MainScreen extends Component<*> {
-	@computed
-	get val() {
-		const {mainStore} = this.props.store;
-		return mainStore.value;
-	}
-	addA = () => {
-		const {a} = this.val;
-		const {mainStore} = this.props.store;
-		mainStore.setValue({a: a + 1});
-	};
-	addB = () => {
-		const {b} = this.val;
-		const {mainStore} = this.props.store;
-		console.log('addB', b);
-		mainStore.setValue({b: b !== undefined ? b + 1 : 0});
-	};
-	render() {
-		const {a, b} = this.val;
-		console.log('render', this.val);
-		return (
-			<View style={styles.container}>
-				<Text style={styles.welcome}>Welcome to React Native!</Text>
-				<Text style={styles.instructions}>To get started, edit App.js</Text>
-				<Button title={'button a'} onPress={this.addA} />
-				<Button title={'button b'} onPress={this.addB} />
-				<Text style={styles.instructions}>{a}</Text>
-				<Text style={styles.instructions}>{b}</Text>
-			</View>
-		);
-	}
+  addA = () => {
+    const { mainStore } = this.props.store;
+    const { a } = mainStore.value;
+    mainStore.setA(a + 1);
+  };
+  addB = () => {
+    const { mainStore } = this.props.store;
+    const { value } = mainStore;
+    const b = has(value, 'b') ? get(value, 'b') : 0;
+    mainStore.setB(b + 1);
+  };
+
+  render() {
+    const { value } = this.props.store.mainStore;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Button title={'button a'} onPress={this.addA} />
+        <Button title={'button b'} onPress={this.addB} />
+        <Text style={styles.instructions}>{get(value, 'a')}</Text>
+        <Text style={styles.instructions}>{get(value, 'b')}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF'
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
 });
